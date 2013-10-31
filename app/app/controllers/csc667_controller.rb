82bb242571418a0b3165a8c6b2a9cf8cd66d9a94
Victor T <vheptam@yahoo.com>
2013-10-30 21:53:33 -0700

    project3 retry

---
+++ app/app/controllers/csc667_controller.rb
@@ -0,0 +1,13 @@
+class Csc667Controller < ApplicationController
+  def index
+  end
+  
+  def project0
+  end
+
+  def project1
+  end
+  
+  def project2
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog2::Application.config.secret_key_base = '618802efcfa593e05f42fc02c263583eca56984b277b53b56732efb402df911f30d5e52cf158f1008cf3f1c79dc0f29536db6acc089347126895a788d81fed71'
